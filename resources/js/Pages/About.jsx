ccebfcda1b9006739b419c599f85c346da405f12
Ayban9417 <82521299+Ayban9417@users.noreply.github.com>
2023-10-27 14:02:14 +0800

    About changes

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -25,7 +25,7 @@ const About = ({ auth }) => {
                         <div className="col-md">
                             <h2 className=' text-uppercase fw-bold'>Bukidnon classic custom voting system</h2>
                             <p className='fw-medium fs-5 mt-3'>
-                                A group of like minded people driven to a passion for retro style motorcycle, custom culture and cafe racing.
+                                A group of like minded people driven to a passion for retro style motorcycle, custom culture and cafe racing. This community represents a close-knit gathering of people who are bound together by their profound love for retro-style motorcycles. Their shared enthusiasm extends beyond mere appreciation; it delves into the intricacies of custom motorcycle culture, where each bike is a canvas for individual expression and craftsmanship. Additionally, their shared zeal for cafe racing, a style known for its blend of speed and style, unites them in the pursuit of a thrilling yet classic riding experience. These individuals don't just own motorcycles; they embrace a unique way of life, one that revolves around the timeless charm of vintage bikes and the camaraderie forged through this shared passion, creating a vibrant and tightly woven subculture within the world of motorcycling.
                             </p>
                         </div>
                     </div>
